366bfb7f4724815aad97a9351fd149572b59ac42
acalidonio <00363918@uca.edu.sv>
2022-11-24 21:05:00 -0600

    Version sin comentarios

--- agregar_palabras.c
+++ agregar_palabras.c
@@ -4,21 +4,33 @@
 
 int main()
 {
-    // se abre el archivo que contendra el resultado
-    FILE *pal = fopen("palabras.txt", "a");
+    // se abren los archivos que contendran las palabras
+    FILE *ez = fopen("facil.txt", "a");
+    FILE *mid = fopen("medio.txt", "a");
+    FILE *hard = fopen("dificil.txt", "a");
 
-    // se confirma que se abrio el archivo correctamente
-    if (pal == NULL)
+    // se confirma que se abrieron los archivos correctamente
+    if (ez == NULL)
+    {
+        printf("ERROR!");
+        return 1;
+    }
+    if (mid == NULL)
+    {
+        printf("ERROR!");
+        return 1;
+    }
+    if (hard == NULL)
     {
         printf("ERROR!");
         return 1;
     }
 
     // se declaran variables, ademas de un string
-    int count, length = 0, nonchar = 1;
+    int count, length = 0, nonchar = 1, space = 0;
     char str[1000];
 
-    while (length < 6 || nonchar > 0)
+    while (length < 6 || nonchar > 0 || space > 0)
     {
         length = 0;
         nonchar = 1;
@@ -62,6 +74,16 @@ int main()
         {
             printf("Por favor ingrese una palabra o frase sin numeros o caracteres especiales\n\n");
         }
+
+        if (isblank(str[0]) || isblank(str[(length - 1)]))
+        {
+            space = 1;
+            printf("Por favor ingrese una palabra o frase sin espacios al inicio o al final\n\n");
+        }
+        else
+        {
+            space = 0;
+        }
     }
 
     // estas tres lineas mueven la palabra del array "str" al array "palabra", el cual tiene la longitud exacta de la palabra o frase utilizada
@@ -70,10 +92,26 @@ int main()
     strncpy(palabra, str, length);
     palabra[length] = '\0';
 
+    printf("La longitud de palabra o frase es %d\n\n", strlen(palabra));
+
+    if (strlen(palabra) > 11 && strlen(palabra) < 20)
+    {
+        fprintf(mid, "%s\n", palabra);
+    }
+    else if(strlen(palabra) > 19)
+    {
+        fprintf(hard, "%s\n", palabra);
+    }
+    else
+    {
+        fprintf(ez, "%s\n", palabra);
+    }
+
     // se imprimen en el documento la palabra o frase
-    fprintf(pal, "%s\n", palabra);
 
     // se cierra el documento
-    fclose(pal);
+    fclose(ez);
+    fclose(mid);
+    fclose(hard);
     return 0;
 }
