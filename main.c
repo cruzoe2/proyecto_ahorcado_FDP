77bb2383fe3364382047018f9bbc8b5cb264e1b7
Tony Rivas <00214522@uca.edu.sv>
2022-11-26 18:36:14 -0600

    haber esto es el avance actual que tenemos

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ void niveldificil();
 int ingresarPalabras();
 void tabla();
 int juego();
-void rotulo(int e);
+void puntaje(int e);
 void gotoxy(short x, short y);
 
 // FUNCION PRINCIPAL MAIN
@@ -111,38 +111,28 @@ int juego()
 
 void nivelFacil()
 {
-	char const s[] = "\n\t";
-	char *token;
-	char arreglo[LONGITUD_MAXIMA_CADENA];
-	int longitud;
-	FILE *archivo = fopen("facil.txt", "r");
-	char bufer[LONGITUD_MAXIMA_CADENA]; // Aquí vamos a ir almacenando cada línea
+	char palabras[50][50], respuesta[40], letra, res, res1;
+	FILE *archivo;
+	int c, i = 0;
+	int longitud, conta, i, j, e, error;
 
-	while (fgets(bufer, LONGITUD_MAXIMA_CADENA, archivo))
+	archivo = fopen("facil.txt", "r"); // abre el archivo en solo lectura
+
+	// toma las palabras linea a linea de el txt mientras que no sea End of File
+	while (c != EOF)
 	{
-		longitud = strlen(bufer);
-
-		token = strtok(bufer, s);
-		// La imprimimos, pero realmente podríamos hacer cualquier otra cosa
-		while (token != NULL)
-		{
-			printf("\n%s\n", token);
-			token = strtok(NULL, s);
-			for (int i = 0; i <= longitud; i++)
-			{
-
-				arreglo[i] = bufer[i];
-				putchar(arreglo[i]);
-			}
-		}
+		c = fscanf(archivo, "%s", palabras[i]); // guardando las palabras en un arreglo
+		i++;
 	}
-
-	printf("\n %d", longitud);
 }
 
-// mostrar los puntajes de cada jugador
-void puntajes()
+// mostrar y guardar el puntaje
+void puntaje(int e)
 {
+	int f = 0;
+	f = 20 - e;
+
+	printf("Usted tiene %d oportunidades restantes. ", f);
 }
 
 // Funcion ingresar palabras
@@ -170,101 +160,143 @@ int ingresarPalabras()
 		return 1;
 	}
 
-	// se declaran variables, ademas de un string lo suficientemente largo
-	int count, length = 0, nonchar = 1, space = 0;
-	char str[1000];
+	int nueva = 1;
 
-	// se hace un loop que mantiene al usuario en la ventana de ingresar frase mientras se cumplan los requisitos
-	// en este caso son, una longitud igual o mayor a 6 caracteres, que solo tenga letras y espacios, y que no hayan espacios al inicio o al final de la palabra o frase
-	while (length < 6 || nonchar > 0 || space > 0)
+	while (nueva == 1)
 	{
-		// se reinician ciertos valores si acaso
-		length = 0;
-		nonchar = 1;
 
-		// se le pide al usuario ingresar una palabra o frase y se guarda en el array "str"
-		printf("Ingrese palabras o frases para incluirlas en el juego, minimo 6 espacios: ");
-		fgets(str, sizeof(str), stdin);
+		// se declaran variables, ademas de un string lo suficientemente largo
+		int count, length = 0, nonchar = 1, space = 0;
+		char str[1000];
 
-		// este es un bucle que permite verificar la longitud de la palabra o frase
-		while (str[length] != '\0')
+		// se hace un loop que mantiene al usuario en la ventana de ingresar frase mientras se cumplan los requisitos
+		// en este caso son, una longitud igual o mayor a 6 caracteres, que solo tenga letras y espacios, y que no hayan espacios al inicio o al final de la palabra o frase
+		while (length < 6 || nonchar > 0 || space > 0)
 		{
-			length++;
-		}
-		// se tiene que restar uno a length ya que el string se almacena con una posicion vacia al final
-		length--;
+			// se reinician ciertos valores si acaso
+			length = 0;
+			nonchar = 1;
 
-		// si hay menos de 6 letras, se pide que se ingrese una palabra o frase con al menos 6 espacios
-		if (length < 6)
-		{
-			printf("Por favor ingrese una palabra o frase con al menos 6 espacios\n\n");
-		}
+			// se le pide al usuario ingresar una palabra o frase y se guarda en el array "str"
+			printf("\nIngrese palabras o frases para incluirlas en el juego, minimo 6 espacios, o ingrese 1 para regresar al menu principal:\n");
+			fgets(str, sizeof(str), stdin);
 
-		// luego, para cada caracter de lo ingresado, se verifica que sean solo letras o espacios, sino se suma uno a nonchar cada vez que se detecta un caracter extraño
-		for (count = 0; count < length; count++)
-		{
-			if (isalpha(str[count]) || isblank(str[count]))
+			// si se selecciona 1, se regresa al menu principal
+			if (str[0] == '1')
 			{
+				// se cierran los documentos
+				fclose(ez);
+				fclose(mid);
+				fclose(hard);
+				menu();
+			}
+
+			// este es un bucle que permite verificar la longitud de la palabra o frase
+			while (str[length] != '\0')
+			{
+				length++;
+			}
+			// se tiene que restar uno a length ya que el string se almacena con una posicion vacia al final
+			length--;
+
+			// si hay menos de 6 letras, se pide que se ingrese una palabra o frase con al menos 6 espacios
+			if (length < 6)
+			{
+				printf("\nPor favor ingrese una palabra o frase con al menos 6 espacios\n");
+			}
+
+			// luego, para cada caracter de lo ingresado, se verifica que sean solo letras o espacios, sino se suma uno a nonchar cada vez que se detecta un caracter extraño
+			for (count = 0; count < length; count++)
+			{
+				if (isalpha(str[count]) || isblank(str[count]))
+				{
+				}
+				else
+				{
+					nonchar++;
+				}
+			}
+			// se tiene que restar uno a nonchar ya que el valor inicial de la variable es 1, y el programa pide que se repita mientras sea superior a 0
+			nonchar--;
+
+			// lineas de depuracion para saber el valor de nonchar y length
+			// printf("\nnonchar es %d\n", nonchar);
+			// printf("length es %d\n\n", length);
+
+			// si se encuentra algo que no sea una letra o espacio, se imprime un mensaje de informacion
+			if (nonchar > 0)
+			{
+				printf("\nPor favor ingrese una palabra o frase sin numeros o caracteres especiales\n");
+			}
+
+			// si se encuentran espacios al inicio o al final de la frase o palabra, se imprime un mensaje de informacion y se reinicia el loop
+			// si no se encuentran espacios al inicio o final, el valor de space es igual a 0 y se guarda la palabra o frase ingresada
+			if (isblank(str[0]) || isblank(str[(length - 1)]))
+			{
+				space = 1;
+				printf("\nPor favor ingrese una palabra o frase sin espacios al inicio o al final\n");
 			}
 			else
 			{
-				nonchar++;
+				space = 0;
 			}
 		}
-		// se tiene que restar uno a nonchar ya que el valor inicial de la variable es 1, y el programa pide que se repita mientras sea superior a 0
-		nonchar--;
 
-		// lineas de sepuracion para saber el valor de nonchar y length
-		// printf("\nnonchar es %d\n", nonchar);
-		// printf("length es %d\n\n", length);
+		// estas tres lineas mueven la palabra del array "str" al array "palabra", el cual tiene la longitud exacta de la palabra o frase utilizada
+		// esto se realiza para que no hayan grandes espacios despues de la palabra o frase que el usuario escribio en el documento de texto
+		char palabra[length];
+		strncpy(palabra, str, length);
+		palabra[length] = '\0';
 
-		// si se encuentra algo que no sea una letra o espacio, se imprime un mensaje de informacion
-		if (nonchar > 0)
+		// otra linea de depuracion para saber la longitud de la nueva string, "palabra"
+		// printf("La longitud de palabra o frase es %d\n\n", strlen(palabra));
+
+		// aqui se determina en que documento se guardaran las palabras o frases, dependiendo de su longitud
+		// si la longitud es superior a 11 e inferior a 20, sera guardado en el documento de dificultad media
+		if (strlen(palabra) > 11 && strlen(palabra) < 20)
 		{
-			printf("Por favor ingrese una palabra o frase sin numeros o caracteres especiales\n\n");
+			fprintf(mid, "%s\n", palabra);
 		}
-
-		// si se encuentran espacios al inicio o al final de la frase o palabra, se imprime un mensaje de informacion y se reinicia el loop
-		// si no se encuentran espacios al inicio o final, el valor de space es igual a 0 y se guarda la palabra o frase ingresada
-		if (isblank(str[0]) || isblank(str[(length - 1)]))
+		// si la longitud es superior a 19, sera guardado en el documento de dificultad dificil
+		else if (strlen(palabra) > 19)
 		{
-			space = 1;
-			printf("Por favor ingrese una palabra o frase sin espacios al inicio o al final\n\n");
+			fprintf(hard, "%s\n", palabra);
 		}
+		// si la longitud es inferior a 12, sera guardado en el documento de dificultad facil
 		else
 		{
-			space = 0;
+			fprintf(ez, "%s\n", palabra);
+		}
+
+		// se declara que nueva sea igual a 0 de nuevo para ingresar en el siguiente loop
+		nueva = 0;
+
+		// este loop sirve para que el usuario seleccione entre agregar una nueva palabra y regresar al menu principal
+		while (nueva != 1)
+		{
+			printf("\nEscriba 1 para ingresar una nueva palabra y 2 para regresar al menu principal: ");
+			scanf("%d", &nueva);
+
+			// si se selecciona 2, se guardan las palabras que se ingresaron y se regresa al menu principal
+			if (nueva == 2)
+			{
+				// se cierran los documentos
+				fclose(ez);
+				fclose(mid);
+				fclose(hard);
+				menu();
+			}
+			// si se selecciona cualquier otro numero, se pide al usuario ingresar o 1 o 2
+			else if (nueva != 2 && nueva != 1)
+			{
+				printf("Por favor ingrese 1 o 2\n");
+				nueva = 0;
+			}
+			// si se ingresa 1, el valor de la variable "nueva" sigue siendo 1
+			else
+			{
+				nueva = 1;
+			}
 		}
 	}
-
-	// estas tres lineas mueven la palabra del array "str" al array "palabra", el cual tiene la longitud exacta de la palabra o frase utilizada
-	// esto se realiza para que no hayan grandes espacios despues de la palabra o frase que el usuario escribio en el documento de texto
-	char palabra[length];
-	strncpy(palabra, str, length);
-	palabra[length] = '\0';
-
-	// otra linea de depuracion para saber la longitud de la nueva string, "palabra"
-	// printf("La longitud de palabra o frase es %d\n\n", strlen(palabra));
-
-	// aqui se determina en que documento se guardaran las palabras o frases, dependiendo de su longitud
-	// si la longitud es superior a 11 e inferior a 20, sera guardado en el documento de dificultad media
-	if (strlen(palabra) > 11 && strlen(palabra) < 20)
-	{
-		fprintf(mid, "%s\n", palabra);
-	}
-	// si la longitud es superior a 19, sera guardado en el documento de dificultad dificil
-	else if (strlen(palabra) > 19)
-	{
-		fprintf(hard, "%s\n", palabra);
-	}
-	// si la longitud es inferior a 12, sera guardado en el documento de dificultad facil
-	else
-	{
-		fprintf(ez, "%s\n", palabra);
-	}
-
-	// se cierran los documentos
-	fclose(ez);
-	fclose(mid);
-	fclose(hard);
 }
